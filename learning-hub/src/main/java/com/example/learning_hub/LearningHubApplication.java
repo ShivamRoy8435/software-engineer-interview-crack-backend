60e0fd4758019eb839c6b7a73687076acd2971a6
ShivamRoy <shivam.roy@kugelblitz.xyz>
2024-08-03 23:49:44 +0530

    setup

---
+++ learning-hub/src/main/java/com/example/learning_hub/LearningHubApplication.java
@@ -0,0 +1,14 @@
+package com.example.learning_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningHubApplication.class, args);
+	}
+
+
+}
